df97f7ad089b318cd18aae59921d922b728db06d
漠北-暗语寒飞 <1223050251@qq.com>
2020-10-20 10:17:45 +0800

    上传gif图片

--- __init__.py
+++ __init__.py
@@ -6,14 +6,14 @@
 RUN_MODE = 'auto'  # manual or auto
 
 '''运行间隔时间(自动模式生效)'''
-UNIT_TIME = 2
+UNIT_TIME = 0.8
 
 '''楼层'''
 MAXIMUM_LAYER = 7
 MINIMUM_LAYER = 1
 
 '''乘客生成概率'''
-PGP = 2  # 最低为1，数值越大，概率越低
+PGP = 3  # 最低为1，数值越大，概率越低
 
 '''电梯最大载重'''
 MAXIMUM_LOAD = 200  # None为默认载重
